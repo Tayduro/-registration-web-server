4bfa6247f4953aa090873d203871806897d6bc08
Tayduro <oleksii.klokol@gmail.com>
2021-12-26 17:05:40 +0200

    1.6

--- index.js
+++ index.js
@@ -1,68 +1,4 @@
-// function errorFirstName() {
-//   const input = document.querySelector('#FirstNameId');
-//   const errorDiv = input.nextSibling.nextSibling;
-//   if (input.value === '') {
-//     errorDiv.innerHTML = '';
-//     return false;
-//   }
-//   if (input.value.length > 255) {
-//     errorDiv.innerHTML = 'max 255';
-//     return false;
-//   }
-//   errorDiv.innerHTML = '';
-//   return true;
-// }
-//
-// function errorLastName() {
-//   const input = document.querySelector('#LastNameId');
-//   const errorDiv = input.nextSibling.nextSibling;
-//   if (input.value === '') {
-//     errorDiv.innerHTML = '';
-//     return false;
-//   }
-//   if (input.value.length > 255) {
-//     errorDiv.innerHTML = 'max 255';
-//     return false;
-//   }
-//   errorDiv.innerHTML = '';
-//   return true;
-// }
-//
-// function passwordError() {
-//   const input = document.querySelector('#passwordId');
-//   const errorDiv = input.nextSibling.nextSibling;
-//   console.log(errorDiv)
-//   if (input.value === '') {
-//     errorDiv.innerHTML = '';
-//     return false;
-//   }
-//   if (input.value.length < 8 || input.value.length > 64) {
-//     errorDiv.innerHTML = 'error';
-//     return false;
-//   }
-//   errorDiv.innerHTML = '';
-//   return true;
-// }
-// function errorEmail() {
-//   const input = document.querySelector('#emailId');
-//   const errorDiv = input.nextSibling.nextSibling;
-//   if (input.value === '') {
-//     errorDiv.innerHTML = '';
-//     return false;
-//   }
-//   if (input.validity.typeMismatch === true) {
-//     errorDiv.innerHTML = 'email is invalid';
-//     return false;
-//   }
-//   errorDiv.innerHTML = '';
-//   return true;
-// }
-
-
 function fieldValidation() {
-  // if (errorEmail() && passwordError() && errorFirstName() && errorLastName()) {
-  //   window.location.href = 'confirmedRegistration.html';
-  // }
   const url = "http://127.0.0.1:8033/login"
 
   const FirstName = document.querySelector('#FirstNameId')
@@ -96,19 +32,6 @@ function fieldValidation() {
 
 
 function ready() {
-
-  const input1 = document.querySelector('#FirstNameId');
-  input1.addEventListener('input', errorFirstName);
-
-  const input2 = document.querySelector('#LastNameId');
-  input2.addEventListener('input', errorLastName);
-
-  const input3 = document.querySelector('#emailId');
-  input3.addEventListener('input', errorEmail);
-
-  const input4 =  document.querySelector('#passwordId');
-  input4.addEventListener('input', passwordError);
-
   const button = document.querySelector('#send-form');
   button.addEventListener('click', fieldValidation);
 }
